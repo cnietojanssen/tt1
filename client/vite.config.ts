2bf028f7f9f0c465c4539adb690a7d360422079b
Cristian Axel Nieto Albayay <cnieto@MacBook-Pro-de-Cristian.local>
2025-04-14 17:39:33 -0400

    fix: output directory correcto para vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: ".", // ya estás dentro de /client
   base: "./",
   plugins: [react()],
   resolve: {
@@ -13,7 +14,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "public", // 👈 DEBE ser "public", no "dist/public"
+    outDir: "public", // 👈 aquí está el fix real
     emptyOutDir: true,
-  },  
+  },
 });
